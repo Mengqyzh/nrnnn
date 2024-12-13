5f1db41c4eb7c24543a478d5737eb04f6a37d8ed
Mengqyzh <mengqyzh@foxmail.com>
2024-12-13 17:54:42 +0800

    Site updated: 2024-12-13 17:54:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/13/hello-world/"];function toRandomPost(){
+var posts=["2024/12/13/hello-world-2/","2024/12/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
